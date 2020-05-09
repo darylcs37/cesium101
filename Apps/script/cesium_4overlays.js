c3c8150ec96836d7d03dc145b2d1e57528e16c26
Dario <64671555+darylcs37@users.noreply.github.com>
2020-05-09 17:22:52 +0800

    Update cesium_4overlays.js

--- Apps/script/cesium_4overlays.js
+++ Apps/script/cesium_4overlays.js
@@ -1,7 +1,7 @@
 	var layers = viewer.imageryLayers;
 
 	var esriPopulationDensity = new Cesium.ArcGisMapServerImageryProvider({
-		url : 'http://agsc.pdc.org/arcgis/rest/services/global/pdc_basemap/MapServer/'
+		url : 'https://agsc.pdc.org/arcgis/rest/services/global/pdc_basemap/MapServer/'
 	});
 	var lyrPopulationDensity = layers.addImageryProvider(esriPopulationDensity);
 	lyrPopulationDensity.alpha = 0.0;
